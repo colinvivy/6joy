75fe27cd7acf4616da5eff6806ec890b3c45c6d9
Seaprince <combbs@gmail.com>
2012-02-26 16:36:37 +0800

    allow multi domains

--- api/config.php
+++ api/config.php
@@ -4,13 +4,15 @@ define('PATH_DATA', PATH_ROOT.'/../appsdata');
 
 $admins = array('seaprince');
 
+$host = $_SERVER['HTTP_HOST'];
+
 // tencent
 $appkey = '801099155';
 $appsecret = '2d65fa8c5e7979ca986c25ba097fa94d';
-$callback = 'http://www.6joy.com/login/t/cb/';
+$callback = "http://$host/login/t/cb/";
 
 // sina
 define( "WB_AKEY" , '176417687' );
 define( "WB_SKEY" , '8faac06d05cabdd592a5315df7021db4' );
-define( "WB_CALLBACK_URL" , 'http://www.6joy.com/login/s/cb/' );
+define( "WB_CALLBACK_URL" , "http://$host/login/s/cb/" );
 
